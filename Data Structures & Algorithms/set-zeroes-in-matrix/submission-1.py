fe46458caa4b0807257dd7fecb223d7c0bd26063
Caelan Hehir <caelanhehir@gmail.com>
2026-07-25 18:18:50 +0200

    Add: set-zeroes-in-matrix - submission-1

---
+++ Data Structures & Algorithms/set-zeroes-in-matrix/submission-1.py
@@ -0,0 +1,22 @@
+class Solution:
+    def target_zone(self, matrix: List[List[int]], target: Tuple[int]) -> None:
+        target_row = target[0]
+        target_col = target[1]
+
+        # Set row to zero
+        for col in range(len(matrix[target_row])):
+            matrix[target_row][col] = 0
+
+        # Set column to zero
+        for row in range(len(matrix)):
+            matrix[row][target_col] = 0
+
+    def setZeroes(self, matrix: List[List[int]]) -> None:
+        targets = []
+        for i in range(len(matrix)):
+            for j in range(len(matrix[i])):
+                if matrix[i][j] == 0:
+                    targets.append((i, j))
+
+        for target in targets:
+            self.target_zone(matrix, target)
